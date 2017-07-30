e1846c61f7ef2dc0718fc31f5b2a6f8863789c50
fabio-henriques <fabio.ghenriques@gmail.com>
2017-07-30 18:12:48 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
   $(window).on('scroll', function(){
     var winT = $(window).scrollTop(),
       winH = $(window).height(),
-      skillsT = skillsDiv.offset().top;
+      skillsT = skillsDiv.offset();
     if(winT + winH  > skillsT){
       $('.skillbar').each(function(){
         $(this).find('.skillbar-bar').animate({
@@ -19,7 +19,7 @@ $( document ).ready(function() {
   
   $('a').click(function() {
     $doc.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+        scrollTop: $( $.attr(this, 'href') ).offset()
     }, 800);
     return false;
   });
