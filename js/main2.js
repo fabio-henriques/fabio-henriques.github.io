0ec860cb2d62a013f1c4b4f89e92d284a90e4fa2
fabio-henriques <fabio.ghenriques@gmail.com>
2017-07-30 18:35:21 +0100

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+
+  var $doc = $('html, body');
+  
+  $('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+    return false;
+  });
+
+});
+/*------------------------scroll-----------------------*/
+
+  
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0},800);
+    return false;
+  });
